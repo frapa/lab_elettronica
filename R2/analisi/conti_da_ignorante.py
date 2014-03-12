ff318fa8a4b32511b04434cb47a9bd7da3ad0c3f
Francesco Pasa <francescopasa@gmail.com>
2014-03-12 17:38:07 +0100

    12345678

--- R2/analisi/conti_da_ignorante.py
+++ R2/analisi/conti_da_ignorante.py
@@ -1,12 +1,16 @@
 # -*- encoding: utf-8 -*-
 
+from math import *
 import numpy as np
 
 R = np.array((19943, 997, 499550))
+dR = np.array((19943, 997, 499550)) * 0.05
 C = np.array((99.3e-9, 990e-9, 5e-9))
+dC = np.array((99.3e-9, 990e-9, 5e-9)) * 0.05
 
 T = R * C
-print(T)
+dT = np.sqrt((dR * C)**2 + (R * dC)**2)
+print(T, dT, sep="\n\n")
 
 C_inc = 3.04e-3 / 3e3
 print(C_inc)
