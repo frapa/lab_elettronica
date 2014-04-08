1d8f033bdd9a06b6e821e7b3d38e477f75541b0e
Francesco Pasa <francescopasa@gmail.com>
2014-04-08 19:27:38 +0200

    83219332098275139239324732423197143272\5939632

--- R6/analisi/diodo.py
+++ R6/analisi/diodo.py
@@ -8,9 +8,16 @@ import matplotlib
 from matplotlib import pyplot as plt
 
 
-dati = np.genfromtxt("../dati/diodo.csv", delimiter=",", skip_header=2)
+dati = np.genfromtxt("../dati/diodo.csv", delimiter=",", skip_header=1)
 I = dati[:,0]
 V = dati[:,1]
+
+ids = np.array([v >= 0 for v in V], dtype=np.bool)
+
+Ip = I[ids]
+Vp = V[ids]
+In = I[np.logical_not(ids)]
+Vn = V[np.logical_not(ids)]
 
 matplotlib.rcParams['font.size'] = 15
 
@@ -23,30 +30,46 @@ f1.suptitle("Caratteristica di un diodo 1N4007",
     y=0.97, fontsize=17)
 
 # GRAFICO 1
-ax1 = f1.add_subplot(1, 1, 1)
+ax1 = f1.add_axes((0.1, 0.12, 0.3, 0.8))
 
 # crea plot con le barre d'errore (o anche senza)
-line = ax1.errorbar(x=V, y=I,
+line = ax1.errorbar(x=Vn, y=In,
     #yerr=dy, #xerr=,
     fmt='o', c='black', linewidth=3,
     zorder=1)
     
-ax1.set_ylabel(u'Tensione [V]',
-    labelpad=12, fontsize=16)
-ax1.set_xlabel(u'Corrente [mA]',
+#ax1.set_xlabel(u'Tensione [V]',
+#    labelpad=12, fontsize=16)
+ax1.set_ylabel(u'Corrente [mA]',
     labelpad=12, fontsize=16)
 
 ax1.grid(True)
 #ax1.set_xscale('log')
-ax1.set_ylim((-50, 300))
-#ax1.set_xlim((-10, 40))
+ax1.set_ylim((-40, 620))
+ax1.set_xlim((-52, 0))
 #for label in ax1.get_xaxis().get_majorticklabels():
 #  label.set_visible(False)
-#ax1.set_yticklabels(("", -25, -20, -15, -10, -5, 0))
+#ax1.set_xticklabels((-50, -40, -30, -20, -10, ""))
+
+ax2 = f1.add_axes((0.4, 0.12, 0.57, 0.8))
+
+for label in ax2.get_yaxis().get_majorticklabels():
+    label.set_visible(False)
+
+line2 = ax2.errorbar(x=Vp, y=Ip,
+    #yerr=dy, #xerr=,
+    fmt='o', c='black', linewidth=3,
+    zorder=1)
+    
+ax2.grid(True)
+ax2.set_ylim((-40, 620))
+ax2.set_xticklabels(("", 0.2, 0.4, 0.4, 0.6, 0.8, 1.0, 1.2))
+
+plt.text(x=0.15, y=-110, s="Tensione [V]")
 
 # questo imposta i bordi del grafico
-f1.subplots_adjust(left=0.12, right=0.97,
+f1.subplots_adjust(left=0.1, right=0.97,
     top=0.92, bottom=0.12, hspace=0.08, wspace=0)
 
 # mostra grafico
-plt.show()  
+plt.show()
