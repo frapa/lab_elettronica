1849c526d4390134bff019e1939ef049e5b64c47
Francesco Pasa <francescopasa@gmail.com>
2014-04-08 18:29:24 +0200

    123

--- R6/analisi/diodo.py
+++ R6/analisi/diodo.py
@@ -12,7 +12,6 @@ dati = np.genfromtxt("../dati/diodo.csv", delimiter=",", skip_header=2)
 I = dati[:,0]
 V = dati[:,1]
 
-
 matplotlib.rcParams['font.size'] = 15
 
 ### PASSA-BASSO
@@ -20,40 +19,30 @@ matplotlib.rcParams['font.size'] = 15
 f1 = plt.figure(figsize=(10, 7), dpi=65)
 
 # Titolo del grafico
-f1.suptitle("Tempo di reazione di un interruttore differenziale",
+f1.suptitle("Caratteristica di un diodo 1N4007",
     y=0.97, fontsize=17)
 
 # GRAFICO 1
 ax1 = f1.add_subplot(1, 1, 1)
 
 # crea plot con le barre d'errore (o anche senza)
-line = ax1.errorbar(x=t, y=v1,
+line = ax1.errorbar(x=V, y=I,
     #yerr=dy, #xerr=,
-    fmt='-', c='red', linewidth=3,
-    zorder=1)
-
-line_corr = ax1.errorbar(x=t, y=v2,
-    #yerr=dy, #xerr=,
-    fmt='-', c='black', linewidth=2,
+    fmt='o', c='black', linewidth=3,
     zorder=1)
     
 ax1.set_ylabel(u'Tensione [V]',
     labelpad=12, fontsize=16)
-ax1.set_xlabel(u'Tempo [ms]',
+ax1.set_xlabel(u'Corrente [mA]',
     labelpad=12, fontsize=16)
 
 ax1.grid(True)
 #ax1.set_xscale('log')
-#ax1.set_ylim((-17, -5))
-ax1.set_xlim((-10, 40))
+ax1.set_ylim((-50, 300))
+#ax1.set_xlim((-10, 40))
 #for label in ax1.get_xaxis().get_majorticklabels():
 #  label.set_visible(False)
 #ax1.set_yticklabels(("", -25, -20, -15, -10, -5, 0))
-
-# questo produce una legenda
-ax1.legend((line, line_corr),
-    ("Tensione ai capi dell'interruttore", "Tensione in ingresso"), 'upper left',
-    prop={'size': 13})
 
 # questo imposta i bordi del grafico
 f1.subplots_adjust(left=0.12, right=0.97,
